f36acfe3db6c38dda2eef1bcef013c6be11841db
Andrey Sapranovich <SapranovichAndrey@yandex.ru>
2020-03-09 17:10:34 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,45 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    'xxxxx': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let n=[];
+    let array =[];
+    expr= expr.split(' ').join('');
+    if(expr.length%10===0){
+      }else n = expr.length%10;
+    array=expr.match(/.{10}/g);
+    array=array.map(x=>x.match(/.{2}/g));
+    
+    for(let k=0; k<array.length; k++){
+    for(let i=0; i<array[k].length; i++){
+       switch (array[k][i]){
+         case '**':
+          array[k].splice(i,1,'x');
+          break;
+          case '10':
+          array[k].splice(i,1,'.');
+          break;
+          case '11':
+          array[k].splice(i,1,'-');
+          break;
+          case '00':
+          array[k].splice(i,1,'');
+          break;
+       }
+    }
+    }
+    let array2=[];
+    for(let l=0;l<array.length; l++){
+      array2+=MORSE_TABLE[array[l].reduce((sum, current) => sum + current)];
+    }
+    for(let z=0; z<n; z++){
+      array2=array2+'0';
+    };
+    return array2;
 }
 
 module.exports = {
